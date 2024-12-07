d680f3405082e7eb6dbfefbcb7d6542199382f54
criminalleader <crazy.angel.priya8051@gmail.com>
2024-12-07 17:26:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,14 +6,14 @@ API_ID = os.environ.get("API_ID", "24638450")
 
 API_HASH = os.environ.get("API_HASH", "b720cf2f8cd64bc0ecee7ea3652a1cd1")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7058431670:AAEJ7MoIcYRQcmHUPRtSjb60ByGWMWCkkRw") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7183574921:AAGy1_Saj6ectTpUuYkH2nCO-V3rFK-Gv0c") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "botupdatesimplifytuber") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "LazyDeveloper") 
 
-DB_NAME = os.environ.get("DB_NAME","Yashkalvar07")     
+DB_NAME = os.environ.get("DB_NAME","lazydev786")     
 
-DB_URL = os.environ.get("DB_URL","mongodb+srv://Yashkalvar07:Yashkalvar07@yashkalvar07.cylum.mongodb.net/?retryWrites=true&w=majority")
- 
+DB_URL = os.environ.get("DB_URL","mongodb+srv://lazy:Zabintkhab7808@lazydev786.lpvunl5.mongodb.net/?retryWrites=true&w=majority")
+
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
 START_PIC = os.environ.get("START_PIC", "https://i.ibb.co/rv8Lds3/ALL-RENAMER-LOGO-YASH-GOYAL.jpg")
@@ -21,10 +21,10 @@ START_PIC = os.environ.get("START_PIC", "https://i.ibb.co/rv8Lds3/ALL-RENAMER-LO
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5965340120 6632019361 5497792868').split()]
 
 # Bot_Username = "@LazyPrincessXBOT"
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "@All_renamer_bot")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@MissDevil_RoBoT")
 MAX_ACTIVE_TASKS = int(os.environ.get("MAX_ACTIVE_TASKS", "5"))
 MAX_FORWARD = int(os.environ.get("MAX_FORWARD", "20"))
-OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "Yash-Goyal")
+OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "LazyDeveloper")
 PORT = os.environ.get('PORT', '8080')
 
 Lazy_session = {}
